0a7effe87f5e15cc9e89bca2af49ed129fbfd972
pereruan <pereruanabaala@gmail.com>
2023-01-12 15:17:59 +0300

    add weather dictionary

--- main.py
+++ main.py
@@ -4,9 +4,24 @@ from flask_sqlalchemy import SQLAlchemy
 
 
 app = Flask(__name__)
+app.config['DEBUG']=True
   
 
 
 @app.route('/')
 def index():
+    url = 'https://api.openweathermap.org/data/2.5/weather?q={}&APPID=7ddd4e724294f24845adb063481c6d43'
+    city='Las Vegas'
+
+    r=requests.get(url.format(city)).json()
+    
+
+    weather={
+        'city': city,
+        'temperature':r['main']['temp'],
+        'description':r['weather'][0]['description'],
+        'icon':r['weather'][0]['icon'],
+    }
+
+    print(weather)
     return render_template('index.html')
