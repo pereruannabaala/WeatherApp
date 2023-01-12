067b83099e67ca56eb9e6279adb15ec705aaf3d7
pereruan <pereruanabaala@gmail.com>
2023-01-12 11:13:16 +0300

    install sqlalchemy

---
+++ main.py
@@ -0,0 +1,12 @@
+import requests 
+from flask import Flask,render_template
+from flask_sqlalchemy import SQLAlchemy
+
+
+app = Flask(__name__)
+  
+
+
+@app.route('/')
+def index():
+    return render_template('index.html')
