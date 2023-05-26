c3c888a14c54ce75d3ccf13342427975ffbe5810
harry <harrisongitau3@gmail.com>
2023-05-27 02:10:44 +0300

    Dev: Add configuration variables needed by the application

--- config.py
+++ config.py
@@ -6,3 +6,6 @@ basedir = os.path.abspath(os.path.dirname(__file__))
 class Config(object):
     # Web form
     SECRET_KEY = os.environ.get('SECRET_KEY') or 'you-will-never-guess'
+
+    # Weather map api
+    WEATHERMAP_API = os.environ.get('WEATHERMAP_API')
