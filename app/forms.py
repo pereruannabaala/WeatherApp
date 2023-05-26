1593160ee63985a74467fd3047da294030b99f81
harry <harrisongitau3@gmail.com>
2023-05-27 02:11:32 +0300

    Fix: Limit the search form for GET requestst only

--- app/forms.py
+++ app/forms.py
@@ -1,6 +1,7 @@
 from flask_wtf import FlaskForm
 from wtforms import StringField
 from wtforms.validators import DataRequired
+from flask import request
 
 
 class SearchForm(FlaskForm):
@@ -8,3 +9,13 @@ class SearchForm(FlaskForm):
         'Search',
         validators=[DataRequired()],
         render_kw={'placeholder': 'Search for a city'})
+
+    def __init__(self, *args, **kwargs):
+        # formdata is where Flask gets form submissions
+        if 'formdata' not in kwargs:
+            # GET request: request.args, POST: request.form
+            kwargs['formdata'] = request.args
+        if 'meta' not in kwargs:
+            # Disable CSRF for clickable link
+            kwargs['meta'] = {'csrf': False}
+        super(SearchForm, self).__init__(*args, **kwargs)
