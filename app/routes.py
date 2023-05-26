a871a1d19aa1181e20394191156a651d322d18b5
harry <harrisongitau3@gmail.com>
2023-05-27 02:11:55 +0300

    Dev: Display weather data

--- app/routes.py
+++ app/routes.py
@@ -1,6 +1,8 @@
 from app import app
-from flask import render_template, url_for, redirect, g, session
+from flask import render_template, url_for, redirect, g, request, \
+    flash, Response, json, session
 from app.forms import SearchForm
+import urllib
 
 # Executed before the a function
 @app.before_request
@@ -11,8 +13,30 @@ def before_request():
 # It will have a form to accept user input (city name)
 # It will process the data needed to show the weather of the city
 # Data will be passed to the templates for display
-@app.route('/', methods=['GET', 'POST'])
-@app.route('/index', methods=['GET', 'POST'])
+@app.route('/')
+@app.route('/index')
 def index():
-    form = SearchForm()
-    return render_template('index.html', title='Weather Data', form=form)
+    city = request.args.get('q')
+    print('City:', city)
+    if city is not None:
+        data = {}
+        data['q'] = city
+        print(data['q'])
+        data['appid'] = app.config['WEATHERMAP_API']
+        data['units'] = 'metrics'
+        url_values = urllib.parse.urlencode(data)
+        url = 'http://api.openweathermap.org/data/2.5/forecast'
+        full_url = url + '?' + url_values
+        data = urllib.request.urlopen(full_url)
+        resp = Response(data)
+        print(resp.status_code)
+        data=json.loads(data.read().decode('utf8'))
+        print(data['list'])
+    else:
+        flash('No city given. Add a city using the search form to see weather details.')
+        data = {}
+    return render_template(
+        'index.html',
+        title=f'Weather Data For {city}',
+        city=city,
+        data=data)
